c336e3f963031169152efa5bdf3b8228d75c29fd
antondashevsky <antondashevsky@gmail.com>
2021-09-17 05:07:38 +0300

    Docker

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 6666;
+var port = process.env.PORT || 8080;
 
 var cors_proxy = require('./src/cors-proksy');
 cors_proxy.createServer({
